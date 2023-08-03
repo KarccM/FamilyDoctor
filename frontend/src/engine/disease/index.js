76c6037c29e904cc399bdacc6202d5a676747434
Kareem <kareemhabtoor@gmail.com>
2023-08-03 15:33:28 +0300

    test engine

--- frontend/src/engine/disease/index.js
+++ frontend/src/engine/disease/index.js
@@ -1,46 +1,49 @@
 class Symptom {
     constructor(name, question) {
-        this.name = name
-        this.question = question
-        this.answers = ['yes', 'no', 'unk']
-        this.answer = undefined
+        this.name = name;
+        this.question = question;
+        this.answers = ['yes', 'no', 'unk'];
+        this.answer = undefined;
     }
 }
 
 
 class Rule {
     constructor(symptoms) {
-        this.symptoms = symptoms
-        this.maxLength = this.symptoms.length
-        this.trueSymptoms = 0
-        this.weight = -999999
+        this.symptoms = symptoms;
+        this.maxLength = this.symptoms.length;
+        this.trueSymptoms = 0;
+        this.weight = -999999;
         this.valid = true;
     }
 
     updateRule(visitedSymptom) {
-        let symptom = this.symptoms.find((e) => visitedSymptom.name === e.name)
-        if (symptom != undefined) {
-            symptom.answer = visitedSymptom.answer
-            if (visitedSymptom.answer === 'yes') {
-                this.trueSymptoms += 1
-                this.weight = this.trueSymptoms - this.maxLength
-            } else if (visitedSymptom.answer === 'no') {
-                this.trueSymptoms = -999999
-                this.weight = -99999//this.trueSymptoms - this.maxLength
-                this.valid = false;
-            }
+        let symptom = this.symptoms.find((e) => visitedSymptom.name === e.name);
+
+        if (symptom == undefined) return;
+
+        symptom.answer = visitedSymptom.answer;
+
+        if (visitedSymptom.answer === 'yes')
+            this.trueSymptoms += 1;
+
+        else if (visitedSymptom.answer === 'no') {
+            this.trueSymptoms = -999999;
+            this.valid = false;
         }
+
+        this.weight = this.trueSymptoms - this.maxLength
     }
 }
 
 
 class Disease {
-    name
-    specialist
-    treatment
-    notes
-    priority
-    rules
+    // name
+    // specialist
+    // treatment
+    // notes
+    // priority
+    // rules
 
     constructor(name, specialist, treatment, notes, priority, rules) {
         this.name = name
@@ -58,19 +61,24 @@ class Disease {
         this.rules = this.rules.filter(r => r.valid)
     }
 
-    // compareFunction(a,b){return sb-a}
+    compareFunction(a, b) { return b - a }
 
     topNSymptoms(n = 1) {
-        // let sortedRules = this.rules.sort((ruleA,ruleB) =>this.compareFunction(ruleA.weight,ruleB.weight))
-        let prominantSymptoms = []
+        let prominantSymptoms = {}
         this.rules.forEach((rule) => {
             rule.symptoms.forEach((symptom) => {
                 if (symptom.answer == undefined) {
-                    prominantSymptoms.push(symptom)
+                    prominantSymptoms[symptom.name] = {
+                        ...symptom,
+                        value: (prominantSymptoms[symptom.name]?.value ?? 0) + 1,
+                    }
                 }
             })
         })
-        return prominantSymptoms[0] ? prominantSymptoms.length > 0 : []
+        // eslint-disable-next-line no-unused-vars
+        let sortedSymptoms = Object.entries(prominantSymptoms).map(([_, value]) => ({ ...value }));
+        sortedSymptoms.sort((symptomA, symptomB) => this.compareFunction(symptomA.value, symptomB.value))
+        return sortedSymptoms.slice(0, n);
     }
 
 }
@@ -78,32 +86,33 @@ class Disease {
 
 
 class Node {
-    disease
-    score
-    tried
+    // disease
+    // score
+    // tried
     constructor(disease) {
-        this.disease = disease
-        this.score = this.disease.priority
-        this.tried = false
+        this.disease = disease;
+        this.score = this.disease.priority;
+        this.tried = false;
+    }
+
+    print() {
+        console.log('this.disease :>> ', this.disease);
+        console.log('this.score :>> ', this.score);
+        console.log('this.tried :>> ', this.tried);
     }
 
     topSymptom() {
-        return this.disease.topNSymptoms()
+        return this.disease.topNSymptoms();
     }
 
     updateRules(visitedSymptom) {
-        this.disease.updateRules(visitedSymptom)
+        this.disease.updateRules(visitedSymptom);
     }
-
-
-
 }
-
-
 
 export {
     Symptom,
     Node,
     Rule,
-    Disease
+    Disease,
 }
--- frontend/src/engine/index.js
+++ frontend/src/engine/index.js
@@ -1,8 +1,51 @@
-import PQueue from "./pq/index.js"
+// import PQueue from "./pq/index.js"
 
-let pq = new PQueue();
-console.log('2', pq.head(2));
-console.log('1', pq.head());
-console.log('5', pq.head(5));
+import { Symptom, Node, Disease, Rule } from "./Disease/index.js"
+
+// let pq = new PQueue();
+// console.log('2', pq.head(2));
+// console.log('1', pq.head());
+// console.log('5', pq.head(5));
 
 // pq.print();
+
+
+let vomit = new Symptom('vomit', 'Do you suffer from vomiting?')
+let nausea = new Symptom('nausea', 'Do you suffer from nausea?')
+let bloating = new Symptom('bloating', 'Do you suffer from bloating?')
+let constipation = new Symptom('constipation', 'Do you suffer from constipation?')
+let lackOfAppetite = new Symptom('lackOfAppetite', 'Do you suffer from lackOfAppetite?')
+let stomacheBurn = new Symptom('StomacheBurn', 'Do you suffer from StomacheBurn?')
+let bloodyStools = new Symptom('bloodyStools', 'Do you suffer from bloodyStools?')
+let fatigue = new Symptom('fatigue', 'Do you suffer from fatigue?')
+let diarrhea = new Symptom('diarrhea', 'Do you suffer from diarrhea?')
+let hyperthermia = new Symptom('hyperthermia', 'Do you suffer from hyperthermia?')
+let earPain = new Symptom('earPain', 'Do you suffer from earPain?')
+let tinnitus = new Symptom('tinnitus', 'Do you suffer from tinnitus?')
+let hearingImpairment = new Symptom('hearningImpairment', 'Do you suffer from hearingImpairment?')
+let headache = new Symptom('headache', 'Do you suffer from headache?')
+let sweating = new Symptom('sweating', 'Do you suffer from sweating?')
+let cough = new Symptom('cough', 'Do you suffer from cough?')
+let musclePain = new Symptom('musclePain', 'Do you suffer from musclePain?')
+let stuffyNose = new Symptom('stuffyNose', 'Do you suffer from stuffyNose?')
+let dysphagia = new Symptom('dysphagia', 'Do you suffer from dysphagia?')
+
+
+// eslint-disable-next-line no-unused-vars
+let symptoms = [vomit, nausea, bloating, constipation, lackOfAppetite, stomacheBurn, bloodyStools,
+  fatigue, diarrhea, hyperthermia, earPain, tinnitus, hearingImpairment, headache,
+  sweating, cough, musclePain, stuffyNose, dysphagia]
+
+let node = new Node(new Disease('كريب', 'dsds', 'sdsds', 'wewewe', 2, [
+  new Rule([vomit, nausea, bloating]),
+  new Rule([diarrhea, hearingImpairment, bloating]),
+  new Rule([diarrhea, nausea, stuffyNose]),
+]))
+
+console.log('node.topSymptom()[0] :>> ', node.topSymptom()[0]);
+let symptom = node.topSymptom()[0];
+node.updateRules({
+  ...symptom,
+  answer: 'yes',
+})
+console.log('node.topSymptom()[0] :>> ', node.topSymptom()[0]);
